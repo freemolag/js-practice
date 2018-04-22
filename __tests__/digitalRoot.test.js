b76398b00195ea00f46fd9efeee7b507df502cbf
freemolag <129sorok@gmail.com>
2018-04-22 14:39:14 +0300

    refactor digitalRoot

--- __tests__/digitalRoot.test.js
+++ __tests__/digitalRoot.test.js
@@ -1,6 +1,6 @@
 import digitalRoot from '../src/codewars/digitalRoot';
 
-it('square every digit of a number', () => {
+it('sum of all the digits in a number', () => {
   expect(digitalRoot(16)).toBe(7);
   expect(digitalRoot(942)).toBe(6);
   expect(digitalRoot(132189)).toBe(6);
