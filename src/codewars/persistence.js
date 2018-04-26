51bf054b36dccc6a1dd0aa03f69d1f7407584a25
freemolag <129sorok@gmail.com>
2018-04-26 13:39:40 +0300

    refactor name

--- src/codewars/persistence.js
+++ src/codewars/persistence.js
@@ -2,8 +2,8 @@ const persistence = (num, acc = 0) => {
   if (num < 10) {
     return acc;
   }
-  const result = String(num).split('').reduce((a, b) => a * b);
-  return persistence(result, acc + 1);
+  const mmultiplyNum = String(num).split('').reduce((a, b) => a * b);
+  return persistence(mmultiplyNum, acc + 1);
 };
 
 export default persistence;
